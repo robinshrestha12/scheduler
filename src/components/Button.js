95bef5abb588495d04536bd64c616dc4c40e3594
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-05-16 17:33:45 -0400

    Building a Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,9 +6,16 @@ export default function Button(props) {
    let buttonClass = "button";
    if (props.confirm) {
       buttonClass += " button--confirm";
-   } 
+   }
    if (props.danger) {
       buttonClass += " button--danger";
    }
-   return <button className={buttonClass}>{props.children}</button>;
+   return (
+      <button
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >{props.children}
+      </button>
+   );
 }
