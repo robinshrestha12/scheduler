1d5e0affda1ccc5d7ee170fe8e1a9df5c0647b37
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-05-19 19:13:48 -0400

    Building Interview Item

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem (props) {
+let interviewerItem = classNames("interviewers__item", {"interviewers__item--selected" : props.selected});
+  return (
+    <li className={interviewerItem} onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected ? <span>{props.name}</span> : <span>""</span> }
+    </li>
+  );
+}
