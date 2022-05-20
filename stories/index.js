edd2343a25d6667dd7e1e925e2ede7fa9563a234
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-05-20 14:50:47 -0400

    more appointment views

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -148,4 +149,5 @@ storiesOf("InterviewerList", module)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
   .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("Delete")}/>)
   .add("Confirm", () => <Confirm message = "Delete the appointment?" onConfirm = {action("onConfirm")} onCancel={action("onCancel")}/>)
-  .add("Status", () => <Status message="deleting"/>);
+  .add("Status", () => <Status message="deleting"/>)
+  .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />);
