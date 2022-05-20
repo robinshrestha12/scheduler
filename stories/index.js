2f52bb18ca64e5cef7d9802b71fc343c4ad35ed3
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-05-20 14:41:21 -0400

    deleting component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -146,4 +147,5 @@ storiesOf("InterviewerList", module)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
   .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("Delete")}/>)
-  .add("Confirm", () => <Confirm message = "Delete the appointment?" onConfirm = {action("onConfirm")} onCancel={action("onCancel")}/>);
+  .add("Confirm", () => <Confirm message = "Delete the appointment?" onConfirm = {action("onConfirm")} onCancel={action("onCancel")}/>)
+  .add("Status", () => <Status message="deleting"/>);
