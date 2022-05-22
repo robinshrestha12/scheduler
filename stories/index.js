6f505e03646dfb16282088cefbe3e27d6367b99d
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-05-22 17:18:35 -0400

    displaying appointment header & show empty 1

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -167,4 +167,11 @@ storiesOf("Appointment", module)
     onSave={action("onSave")}
     onCancel={action("onCancel")}
     //onLoad={action("onChange")}
-  />);
+  />)
+
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ));
