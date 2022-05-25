4cb65a4eca11a53a5550c88a518f341c37825b1b
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-05-25 13:01:49 -0400

    requesting the days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "components/Application.scss";
+import axios from "axios";
 
-//mock data
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 const appointments = {
   "1": {
     id: 1,
@@ -64,6 +48,15 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:8001/api/days`;
+    axios.get(url).then(response => {
+      console.log(response);
+     setDays([...response.data]);
+    })
+  }, [days]);
 
   const schedule = Object.values(appointments).map((appointment) => {
     return (
